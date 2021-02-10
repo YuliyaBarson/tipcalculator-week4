a3f2ea819adaf46b9129e24126d7476ed3bfc9b6
Yuliya Barson <200468772@student.georgianc.on.ca>
2021-02-10 17:08:19 -0500

    fixed syntax error/ added button

--- calculate-script.php
+++ calculate-script.php
@@ -1,19 +1,27 @@
 <?php
 
 //create variables to store info 
-$bill_total = filter_input(INPUT_POST, 'amount');
-$tip_percentage = filter_input(INPUT_POST, 'percentage')
+
 $submit = filter_input(INPUT_POST, 'submit'); 
 
 //create a function to calculate the tip percentage and return 
 
-function tipCalculate($total, $percentage){ 
-    $total_tip = $total * $percentage;
-    echo "<p> If your bill is $total and you'd like to tip $percentage, you should tip $total_tip</p>';
-}
-//if submit is set, invoke the function with the required arguments 
-if(isset($submit)) {
-tipCalculate($bill_total, $tip_Percentage);
+
+function tipCalculate($total, $percentage){  
+    $total_tip = $total * ($percentage * 0.01);
+    echo "<p> If your bill is $total and you'd like to tip $percentage, you should tip $total_tip</p>";
 }
 
+
+//if submit is set, invoke the function with the required arguments 
+if(isset($submit)){
+    $bill_total = filter_input(INPUT_POST, 'amount', FILTER_VALIDATE_FLOAT);
+    $tip_percentage = filter_input(INPUT_POST, 'percentage');
+    if($bill_total === false){
+        echo "<p> Please enter a numeric value!</p>";
+    }
+    else{   
+        tipCalculate($bill_total, $tip_percentage);
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -37,6 +37,7 @@
                         <option value="35">35%</option>
                     </select>
                 </div>
+                <input type="submit" value="submit" name="submit" class="btn btn-secondary">
             </form>
             <button name="clear" class="btn btn-secondary"> clear calculator </button>
             <?php require('calculate-script.php'); ?>
